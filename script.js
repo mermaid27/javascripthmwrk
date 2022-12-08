a54e464b7379a4bd4a49aea106a5cd31233d6040
mermaid27 <n.ghurbela@gmail.com>
2022-12-08 18:06:09 +0000

    git

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+// let inputfield = document.getElementById ("btn-wraper"); 
+//     inputfield ("keydown", function(event) {
+//     console.log(event)
+//     if (event.Keycode == 13) {
+//         alert  ("you pressed enter")
+//     }
+//     else {
+//         console.log ("hellow"); 
+//     }
+// }
+
+// )
+
+// let event = {
+//     keycode: 13 
+// }
+
+// inputfield.addEventListener ("keydown", function (Event) {
+//     Event.classList.add('inputclass');
+// }
+// ) 
+
+// let link = document.getElementById('link');
+// link.addEventListener('click', function(event) {
+//     event.preventDefault(); 
+// }
+              
+// )
+
+let ulitem = document.querySelector('.ul-list')
+let liitem = document.createElement('li')
+liitem.textContent ='Javascript'; 
+ulitem.appendChild('liitem'); 
+
+
